b025dceb4eb3e7216a330b1fc959a82db911ec8a
Isaac Hermel <isaachermel@gmail.com>
2023-03-31 13:50:04 -0300

    Static Generation w/data

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() => console.log("Script loaded successfully!")}
+        />
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </Layout>
+  );
+}
